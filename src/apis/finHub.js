52517a8d2aac054f4dfb3d2ed51b8cc39072fb64
Lemonsprig <ianselbyuk@gmail.com>
2023-01-20 12:42:22 +0000

    Fetch data implemented.

--- src/apis/finHub.js
+++ src/apis/finHub.js
@@ -1,5 +1,10 @@
 import axios from 'axios';
 
+const TOKEN = 'cf4laqaad3i7dbfhr8a0cf4laqaad3i7dbfhr8ag'
+
 export default axios.create({
-  baseURL: 'https://finnhub.io/api/v1'
+  baseURL: 'https://finnhub.io/api/v1',
+  params: {
+    token: TOKEN
+  }
 })
--- src/components/StockList.jsx
+++ src/components/StockList.jsx
@@ -1,6 +1,41 @@
 import { useState, useEffect} from 'react';
+import finHub from '../apis/finHub';
 
 export const StockList = () => {
+  const [stock, setStock] = useState([]);
   const [watchList, setWatchList] = useState(['MSFT', 'AMZN', 'GOOGL']);
+
+  useEffect(() => {
+    let isMounted = true;
+    const fetchData = async () => {  
+      try {
+        const responses = await Promise.all(watchList.map((stock) => {
+          return finHub.get("/quote", {
+            params: {
+              symbol: stock
+            }
+          });
+        }));
+        
+        const data = responses.map((response) => {
+          return {
+            data: response.data,
+            symbol: response.config.params.symbol
+          }
+        })
+        if(isMounted) {
+          setStock(data);
+        }
+        console.log(data);
+      } catch (err) {
+
+      }
+    }
+    fetchData();
+
+    return () => (isMounted = false)
+
+  }, [])
+
   return <div>Stock List Component</div>
 }
