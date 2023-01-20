9c6db9bea9174cfc2250c385710ef2bffdac469a
Lemonsprig <ianselbyuk@gmail.com>
2023-01-20 13:37:09 +0000

    Rendered stock data to table.

--- src/components/StockList.jsx
+++ src/components/StockList.jsx
@@ -1,9 +1,19 @@
 import { useState, useEffect} from 'react';
+import {BsFillCaretDownFill} from 'react-icons/bs'
+import {BsFillCaretUpFill} from 'react-icons/bs'
 import finHub from '../apis/finHub';
 
 export const StockList = () => {
   const [stock, setStock] = useState([]);
   const [watchList, setWatchList] = useState(['MSFT', 'AMZN', 'GOOGL']);
+
+  const changeColor = (change) => {
+    return change > 0 ? 'success' : 'danger';
+  }
+
+  const renderIcon = (change) => {
+    return change > 0 ? <BsFillCaretDownFill/> : <BsFillCaretDownFill/>;
+  }
 
   useEffect(() => {
     let isMounted = true;
@@ -37,5 +47,36 @@ export const StockList = () => {
 
   }, [])
 
-  return <div>Stock List Component</div>
+  return <div>
+    <table className="table table-hover mt-5">
+      <thead style={{color: "rgb(79,89,102)" }}> 
+      <tr>
+        <th scope="col">Name</th>
+        <th scope="col">Last</th>
+        <th scope="col">Chg</th>
+        <th scope="col">Chg%</th>
+        <th scope="col">High</th>
+        <th scope="col">Low</th>
+        <th scope="col">Open</th>
+        <th scope="col">Pclose</th>
+      </tr>
+      </thead>
+      <tbody>
+        {stock.map((stockData) => {
+          return (
+            <tr className='table-row' key={stockData.symbol}>
+              <th scope="row">{stockData.symbol}</th>
+              <td>{stockData.data.c}</td>
+              <td className={`text-${changeColor(stockData.data.d)}`}>{stockData.data.d}{renderIcon(stockData.data.d)}</td>
+              <td className={`text-${changeColor(stockData.data.dp)}`}>{stockData.data.dp}{renderIcon(stockData.data.dp)}</td>
+              <td>{stockData.data.h}</td>
+              <td>{stockData.data.l}</td>
+              <td>{stockData.data.o}</td>
+              <td>{stockData.data.pc}</td>
+            </tr>   
+          )
+        })}
+      </tbody>
+    </table>
+  </div>
 }
