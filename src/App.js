f2549ff775e03130df2604f9b414e7065d4f578f
Lemonsprig <ianselbyuk@gmail.com>
2023-01-22 11:36:09 +0000

    Added autocomplete population and watchListContext.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import { StockDetailPage } from './pages/StockDetailPage';
 import { StockOverviewPage } from './pages/StockOverviewPage';
 import './App.css';
+import { WatchListContextProvider } from './context/watchListContext';
 
 
 const finHubAPIKey  = 'cf4laqaad3i7dbfhr8a0cf4laqaad3i7dbfhr8ag'
@@ -9,12 +10,14 @@ const finHubAPIKey  = 'cf4laqaad3i7dbfhr8a0cf4laqaad3i7dbfhr8ag'
 function App() {
   return (
     <main className='container'>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<StockOverviewPage />}></Route>
-          <Route path="/detail/:symbol" element={<StockDetailPage />}></Route>
-        </Routes>
-      </BrowserRouter>
+      <WatchListContextProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<StockOverviewPage />}></Route>
+            <Route path="/detail/:symbol" element={<StockDetailPage />}></Route>
+          </Routes>
+        </BrowserRouter>
+      </WatchListContextProvider>
     </main>
   );
 }
--- src/components/AutoComplete.jsx
+++ src/components/AutoComplete.jsx
@@ -1,3 +1,62 @@
+import { render } from '@testing-library/react';
+import { useState, useEffect} from 'react';
+import finHub from '../apis/finHub';
+
 export const AutoComplete = () => {
-  return <div>AutoComplete Component</div>
+  const [search, setSearch] = useState('');
+  const [results, setResults] = useState([]);
+
+  const renderDropdown = () => {
+    const dropDownClass = search ? 'show' : null;
+    return (
+      <ul className={`dropdown-menu ${dropDownClass}`}
+        style={{
+          height: "500px",
+          overflowY: "scroll",
+          overflowX: "hidden",
+          cursor: "pointer"
+        }}> 
+        {results.map((result) => {
+          return (
+            <li key={result.symbol} className='dropdown-item'>{result.description} ({result.symbol})</li>
+          )
+        })}
+      </ul>
+    )
+  }
+
+  useEffect(()=>{
+    let isMounted = true;
+    const fetchData = async() => {
+      try {
+        const response = await finHub.get('/search', {
+          params: {
+            q: search
+          }
+        })
+        if(isMounted) {
+          setResults(response.data.result);
+        }
+      } catch (error) {
+
+      }
+    }
+    if (search.length > 0) {
+      fetchData();
+    } else {
+      setResults([]);
+    }
+    return () => (isMounted = false);
+    
+  }, [search]);
+
+  return <div className="w-50 p-5 rounded mx-auto">
+    <div className="form-floating dropdown">
+      <input type="text" style={{backgroundColor:"rgba(145, 158, 171, 0.04"}} 
+        id="search" className="form-control" placeholder="Search"
+        autoComplete='off' value={search} onChange={(e) => setSearch(e.target.value)}/>
+      <label htmlFor="search">Search</label>
+      {renderDropdown()}
+    </div>
+  </div>
 }
